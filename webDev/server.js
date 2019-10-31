967fa511f6635a7ce4ac03f5b60079d929c4cb6f
Caio Cid Santiago <caiocid@gmail.com>
2019-10-31 13:02:07 -0300

    WebDev 06 (incomplete)
    Use of Multer and changing JSON datas

--- webDev/server.js
+++ webDev/server.js
@@ -5,11 +5,13 @@ const expressLayouts = require('express-ejs-layouts');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const path = require('path');
+const multer = require('multer');
 
 var app = express();
 
 const hostname = '127.0.0.1';
 const port = 8080;
+const maxSize = 0.5*1024*1024; // 0.5MB
 
 app.set('view engine','ejs');
 app.use(express.static("public"));
@@ -52,8 +54,74 @@ app.get('/book/:usu', function (req, res) {
     res.render('book', data);
 });
 
+app.get('/edit/:usu', function (req, res) {
+	var dirData = path.join(__dirname+'/public/data/livros/'+req.params.usu+'/data.json');
+	var data = JSON.parse(fs.readFileSync(dirData));
+    res.render('editbook', data);
+});
+
 app.get('/config', function (req, res) {
     res.render('config');
+});
+
+// Verificar utilização do :usu
+app.post('/edit/:bookid/upload', function (req, res) {
+	
+	var bookImgstorage = multer.diskStorage({
+		// destino do arquivo
+		destination: function (req, file, cb) {
+			cb(null, '/public/data/livros/'+ req.params.bookid)
+		},
+		// nome do arquivo
+		filename: function (req, file, cb) {
+			cb(null, bookid + '.jpg');
+		}
+	});
+	
+	// utiliza a storage para configurar a instância do multer
+	var bookImgupload = multer({
+		storage : bookImgstorage,
+		limits  : { fileSize: maxSize }
+	});
+
+	bookImgupload.single('formimglivro') ;
+
+	var direct = path.join(__dirname + '/public/data/livros/'+req.params.bookid);
+	
+	// Faltar atualizar loja no JSON
+	var new_data = JSON.stringify({
+		id: req.body.formid,
+    	nomelivro: req.body.formnomelivro,
+    	nomeautor: req.body.formnomeautor,
+    	desclivro: req.body.formdesclivro,
+    	nomeusuario: req.body.formnomeusuario,
+    	genero: req.body.formgenero,
+    	numpaginas: req.body.formnumpaginas,
+    	editora: req.body.formeditora,
+    	idioma: req.body.formidioma,
+    	ISBN10: req.body.formISBN10,
+    	ISBN13: req.body.formISBN13,
+		peso: req.body.formpeso,
+	});
+	
+	var new_short = JSON.stringify({
+		id: req.body.formid,
+		nomelivro: req.body.formnomelivro
+	});
+
+	fs.writeFile(direct + '/data.json', new_data, function (err, data){
+		if (err) {
+			console.log('Erro gravando o arquivo data.json');
+			return console.error(err);
+		}
+	});
+
+	fs.writeFile(direct + '/short.json', new_short, function (err, data){
+		if (err) {
+			console.log('Erro gravando o arquivo short.json');
+			return console.error(err);
+		}
+	});
 });
 
 app.listen( port, hostname, function () {
