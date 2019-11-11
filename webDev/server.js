e391e126b4de01e44dba3be899b95a3709e5a83d
Caio Cid Santiago <caiocid@gmail.com>
2019-11-11 13:09:35 -0300

    WebDev 07
    Added function to create new book and delete

--- webDev/server.js
+++ webDev/server.js
@@ -1,5 +1,5 @@
 //Server using Server-side proccessing
-const fs = require('fs');
+const fs = require('fs-extra');
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts'); 
 const bodyParser = require('body-parser');
@@ -64,7 +64,191 @@ app.get('/config', function (req, res) {
     res.render('config');
 });
 
-// Verificar utilização do :usu
+app.get('/create', function (req, res) {
+	res.render('createbook');
+});
+
+app.get('/delet/:bookid', function (req, res) {
+	var dirData = path.join(__dirname+'/public/data/livros/'+req.params.bookid+'/short.json');
+	var short = JSON.parse(fs.readFileSync(dirData)); 
+	res.render('deletbook',short);
+});
+
+
+app.get('/delet/:bookid/confirmed', function (req, res) {
+	var dirData = path.join(__dirname+'/public/data/livros/'+req.params.bookid);
+	var dirBin = path.join(__dirname+'/public/bin/'+req.params.bookid)
+	
+	if (fs.existsSync(dirBin)) {
+		fs.removeSync(dirBin);
+	}
+
+	fs.move(dirData,dirBin, function (err) {
+		if (err) {
+			return console.log(err);
+		}
+		console.log("Folders moved!");
+	});
+	
+	if (req.cookies.lastBook == req.params.bookid) {
+		res.clearCookie('lastBook');
+	}
+
+	res.send('<h2>Exclusão realizada com sucesso! </h2>'+
+	'<a href="/">Voltar para home</a>')
+});
+
+app.post('/create/upload', function (req, res) {
+
+	var upload = path.join(__dirname + '/public/uploads');
+
+	var dataStorage = multer.diskStorage({
+		// destino do arquivo
+		destination: function (req, file, cb) {
+			cb(null, upload)
+		},
+		// nome do arquivo
+		filename: function (req, file, cb) {
+			cb(null, file.fieldname+'.jpg');
+		}
+	});
+	
+	// utiliza a storage para configurar a instância do multer
+	var dataUpload = multer({
+		storage : dataStorage,
+		limits  : { fileSize: maxSize }
+	}).fields([
+		{name: 'formimglivro'},
+		{name: 'formimgautor'},
+		{name: 'formimgloja'},
+		{name: 'formimgusuario'}
+	]);
+
+	dataUpload(req, res, function (err) {
+		if (err) {
+			res.send(' <h2>O seu upload NÃO foi realizado! </h2> <br>'+
+			'erro: '+ err.message +'<br>'+
+			'<a href="/">Voltar para home</a>');
+            return console.log(err);
+		}
+		
+		var bookid = req.body.formidlivro;
+		var direct = path.join(__dirname + '/public/data/livros/'+bookid);
+		
+		if (fs.existsSync(upload+'/formimglivro.jpg')) {
+			fs.mkdir(direct);
+			fs.renameSync(upload+'/formimglivro.jpg',direct+'/'+bookid+'.jpg');
+		}
+
+		var autor = req.body.formnomeautor.split(" ");
+		var dirAutor = path.join(__dirname + '/public/data/autores');
+		if (fs.existsSync(upload+'/formimgautor.jpg')) {
+			fs.renameSync(upload+'/formimgautor.jpg',dirAutor+'/'+autor[0]+'.jpg');
+		}
+		
+		var loja = req.body.formlojanome.split(" ");
+		var dirLoja = path.join(__dirname + '/public/data/lojas');
+		if (fs.existsSync(upload+'/formimgloja.jpg')) {
+			fs.renameSync(upload+'/formimgloja.jpg',dirLoja+'/'+loja[0]+'.jpg');
+		}
+
+		var usuario = req.body.formusuarionome.split(" ");
+		var dirUsuario = path.join(__dirname + '/public/data/usuarios');
+		if (fs.existsSync(upload+'/formimgusuario.jpg')) {
+			fs.renameSync(upload+'/formimgusuario.jpg',dirUsuario+'/'+usuario[0]+'.jpg');
+		}
+		
+		var data = {
+		"id":" ",
+		"nomelivro":" ",
+		"nomeautor": " ",
+		"imglivro": " ",
+		"imgautor": " ",
+		"desclivro":" ",
+		"nomeusuario": " ",
+		"imgusuario": " ",
+		"genero": " ",
+		"numpaginas": " ",
+		"editora": " ",
+		"idioma": " ",
+		"ISBN10": " ",
+		"ISBN13": " ",
+		"peso": " ",
+		"lojas": " ",
+		"livrosemelhantes": " ",
+		"avalmedia":" ",
+		"resenhas":" "
+		};
+
+		data.id = bookid;
+		data.nomelivro = req.body.formnomelivro;
+		data.nomeautor = req.body.formnomeautor;
+		data.imglivro = "/data/livros/"+bookid+'/'+bookid+'.jpg';
+		data.imgautor = "/data/autores/"+autor[0]+'.jpg';
+		data.desclivro = req.body.formdescricao;
+		data.genero = req.body.formgenero;
+		data.numpaginas = req.body.formnumpaginas;
+		data.editora = req.body.formeditora;
+		data.idioma = req.body.formidioma;
+		data.ISBN10 = req.body.formISBN10;
+		data.ISBN13 = req.body.formISBN13;
+		data.peso = req.body.formpeso;
+		data.lojas = [ {
+			"nome": req.body.formlojanome, 
+            "img": "/data/lojas/"+loja[0]+'.jpg', 
+            "fisica": req.body.formlojafisica, 
+            "digital": req.body.formlojadigital,
+            "capAval": req.body.formlojacapAval, 
+			"localizacao": req.body.capAval, 
+            "aval": ""
+		}];
+		data.nomeusuario = req.body.formusuarionome;
+		data.imgusuario = "/data/usuarios/"+usuario[0]+'.jpg';
+		data.resenhas = [];
+		
+		if (fs.existsSync(path.join(__dirname + '/public/data/livros/'+req.body.formsemelid))){
+			var dir = path.join(__dirname + '/public/data/livros/'+req.body.formsemelid+'/short.json')
+			var semelShort = JSON.parse(fs.readFileSync(dir));
+			var short = {
+				"nome": semelShort.nomelivro,
+				"url": semelShort.id,
+				"img":semelShort.imglivro
+			}
+			data.livrosemelhantes = [short];
+		}
+
+
+		short = { 
+			"nomelivro": "",
+			"imglivro": "",
+			"avalmedia":"",
+			"id":""
+		}
+		
+		short.nomelivro = req.body.formnomelivro;
+		short.imglivro = "/data/livros/"+bookid+'/'+bookid+'.jpg';
+		short.id = req.body.formidlivro;
+
+		fs.writeFile(direct + '/data.json', JSON.stringify(data), function (err, data){
+			if (err) {
+				console.log('Erro gravando o arquivo data.json');
+				return console.error(err);
+			}
+		});
+
+		fs.writeFile(direct + '/short.json', JSON.stringify(short), function (err, data){
+			if (err) {
+				console.log('Erro gravando o arquivo short.json');
+				return console.error(err);
+			}
+		});
+
+		res.send('<h2>Upload realizado com sucesso! </h2>'+
+        '<a href="/">Voltar para home</a>');
+	});
+});
+
+
 app.post('/edit/:bookid/upload', function (req, res) {
 	
 	var bookid = req.params.bookid;
